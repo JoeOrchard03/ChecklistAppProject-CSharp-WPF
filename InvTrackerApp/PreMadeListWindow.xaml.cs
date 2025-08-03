3bf7ae8988110f6301c2cc689c70868b6320838b
Joe Orchard <JO278870@falmouth.ac.uk>
2025-08-03 16:55:01 +0100

    Made new window for pre-made lists and json for storage
    Need to confiugure pre-made list item padding to make it look better and add some data to visualise when working in VS

--- InvTrackerApp/PreMadeListWindow.xaml.cs
+++ InvTrackerApp/PreMadeListWindow.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System.Collections.ObjectModel;
 using System.Diagnostics;
+using System.IO;
+using System.Text.Json;
 using System.Windows;
 using static InvTrackerApp.Helper;
 
@@ -8,15 +10,36 @@ namespace InvTrackerApp
 {
     public partial class PreMadeListWindow : Window
     {
+        public ObservableCollection<PreMadeList> preMadeLists = new ObservableCollection<PreMadeList>();
+        
         public PreMadeListWindow()
         {
             InitializeComponent();
+            preMadeLists = LoadPreMadeLists("PreMadeLists.json");
+            PreMadeLists.ItemsSource = preMadeLists;
+            Debug.WriteLine("PreMadeListWindow Loaded");
+            foreach (var list in preMadeLists)
+            {
+                Debug.WriteLine("list name is: " + list.Name);
+            }
         }
 
         private void GoBack_Click(object sender, RoutedEventArgs e)
         {
             Window newWindow = new MainWindow();
-            Helper.OpenAtSamePosition(this, newWindow);
+            OpenAtSamePosition(this, newWindow);
+        }
+
+        private ObservableCollection<PreMadeList> LoadPreMadeLists(string filePath)
+        {
+            string json = File.ReadAllText(filePath);
+            var lists = JsonSerializer.Deserialize<List<PreMadeList>>(json);
+            return new ObservableCollection<PreMadeList>(lists ?? new List<PreMadeList>());
+        }
+
+        private void ListAddButton_Click(object sender, RoutedEventArgs e)
+        {
+            Debug.WriteLine("List add button clicked");
         }
     }
 }
---
+++ InvTrackerApp/PreMadeLists.cs
@@ -0,0 +1,7 @@
+﻿using System.Collections.Generic;
+
+public class PreMadeList
+{
+    public string Name { get; set; } = string.Empty;
+    public List<string> Items { get; set; } = new();
+}
